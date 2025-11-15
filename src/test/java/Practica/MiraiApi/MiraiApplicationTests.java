18ac63af8cf72a2055cc01cdb1d2acbc36587cda
Oscar Meneses <luis.meneses.arirama@gmail.com>
2025-11-15 03:38:46 -0500

    test de integracion

---
+++ src/test/java/Practica/MiraiApi/MiraiApplicationTests.java
@@ -0,0 +1,12 @@
+package Practica.MiraiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
