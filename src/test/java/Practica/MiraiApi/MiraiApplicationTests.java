ede3c5140a65aa294fc1fefe1fde05a739f5b966
Oscar Meneses <luis.meneses.arirama@gmail.com>
2025-11-15 03:49:42 -0500

    test de integracion con dependencias en memoria

--- src/test/java/Practica/MiraiApi/MiraiApplicationTests.java
+++ src/test/java/Practica/MiraiApi/MiraiApplicationTests.java
@@ -2,11 +2,14 @@ package Practica.MiraiApi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MiraiApiApplicationTests {
 
     @Test
     void contextLoads() {
+        
     }
 }
